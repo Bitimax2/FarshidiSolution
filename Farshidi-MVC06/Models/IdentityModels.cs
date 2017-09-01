58549cfe7aebe419c0cf156c20096e80210b238a
Tajeddin <Behdad_Tajeddin@hotmail.com>
2017-09-01 14:44:59 +0430

    Creating Person Class -Add to dbContext

--- Farshidi-MVC06/Models/IdentityModels.cs
+++ Farshidi-MVC06/Models/IdentityModels.cs
@@ -24,7 +24,7 @@ namespace Farshidi_MVC06.Models
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
-
+        public DbSet<Person> Persons { get; set; }
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
---
+++ Farshidi-MVC06/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Farshidi_MVC06.Models
+{
+    public class Person
+    {
+
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string ContactNo1 { get; set; }
+        public string ContactNo2 { get; set; }
+        public string Email { get; set; }
+        public string WebSite { get; set; }
+        public string CountryName { get; set; }
+        public string Address{ get; set; }
+
+    }
+}
